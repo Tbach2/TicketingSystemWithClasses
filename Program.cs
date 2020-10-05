6ee33797aef9985752dcddbc0d314d0253360bd2
Tyler Bach <Tbach2@my.wctc.edu>
2020-10-04 22:35:20 -0700

    Program class updated

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace TicketingSystem
 {
     class Program
     {
+        // create static instance of Logger
         private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
         static void Main(string[] args)
         {
@@ -14,6 +15,18 @@ namespace TicketingSystem
             logger.Info("Program started");
 
             TicketFile ticketFile = new TicketFile(ticketFilePath);
+
+            string choice = "";
+            do
+            {
+                // display choices to user
+                Console.WriteLine("1) Add Ticket");
+                Console.WriteLine("2) Display All Tickets");
+                Console.WriteLine("Enter to quit");
+                // input selection
+                choice = Console.ReadLine();
+                logger.Info("User choice: {Choice}", choice);
+            } while (choice == "1" || choice == "2");
 
             logger.Info("Program ended");
         }
