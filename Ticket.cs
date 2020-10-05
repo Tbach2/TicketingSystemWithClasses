50c90d84a7233587be123e662637ef81b6ecea26
Tyler Bach <Tbach2@my.wctc.edu>
2020-10-04 22:15:40 -0700

    Ticket class added

---
+++ Ticket.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace TicketingSystem
+{
+    public class Movie
+    {
+        public UInt64 ticketId { get; set; }
+        public string summary { get; set; }
+        public string status { get; set; }
+        public string priority { get; set; }
+        public string submitter { get; set; }
+        public string assigned { get; set; }
+        public List<string> watching { get; set; }
+    }
+}
